cb6cb383a78e97c8003e935172ccf0e8cdf7f1d9
git senpai <satyamshukla.dev@gmail.com>
2024-12-03 18:51:38 +0530

    fixed bugs

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
